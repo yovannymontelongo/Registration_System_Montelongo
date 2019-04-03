a79deeae582bfbef5f1f8398435369922abb5bdc
Yovanny Montelongo <pickleyova@live.com>
2019-04-02 20:26:24 -0500

    Added Student Update Form

---
+++ Registration_System_Montelongo/Student_Update_Form.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Registration_System_Montelongo {
+    partial class Student_Update_Form {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.degreeTextBox = new System.Windows.Forms.TextBox();
+            this.degreeLabel = new System.Windows.Forms.Label();
+            this.majorTextBox = new System.Windows.Forms.TextBox();
+            this.majorLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lnameTextBox = new System.Windows.Forms.TextBox();
+            this.lnameLabel = new System.Windows.Forms.Label();
+            this.fnameLabel = new System.Windows.Forms.Label();
+            this.fnameTextBox = new System.Windows.Forms.TextBox();
+            this.studentComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(200, 265);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(89, 23);
+            this.exitButton.TabIndex = 16;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(23, 265);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(89, 23);
+            this.submitButton.TabIndex = 14;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // degreeTextBox
+            // 
+            this.degreeTextBox.Location = new System.Drawing.Point(115, 184);
+            this.degreeTextBox.Name = "degreeTextBox";
+            this.degreeTextBox.Size = new System.Drawing.Size(160, 20);
+            this.degreeTextBox.TabIndex = 13;
+            // 
+            // degreeLabel
+            // 
+            this.degreeLabel.AutoSize = true;
+            this.degreeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.degreeLabel.Location = new System.Drawing.Point(36, 185);
+            this.degreeLabel.Name = "degreeLabel";
+            this.degreeLabel.Size = new System.Drawing.Size(54, 16);
+            this.degreeLabel.TabIndex = 18;
+            this.degreeLabel.Text = "Degree";
+            // 
+            // majorTextBox
+            // 
+            this.majorTextBox.Location = new System.Drawing.Point(115, 158);
+            this.majorTextBox.Name = "majorTextBox";
+            this.majorTextBox.Size = new System.Drawing.Size(160, 20);
+            this.majorTextBox.TabIndex = 11;
+            // 
+            // majorLabel
+            // 
+            this.majorLabel.AutoSize = true;
+            this.majorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.majorLabel.Location = new System.Drawing.Point(36, 159);
+            this.majorLabel.Name = "majorLabel";
+            this.majorLabel.Size = new System.Drawing.Size(42, 16);
+            this.majorLabel.TabIndex = 17;
+            this.majorLabel.Text = "Major";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(292, 33);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Student Update Form";
+            // 
+            // lnameTextBox
+            // 
+            this.lnameTextBox.Location = new System.Drawing.Point(115, 132);
+            this.lnameTextBox.Name = "lnameTextBox";
+            this.lnameTextBox.Size = new System.Drawing.Size(160, 20);
+            this.lnameTextBox.TabIndex = 10;
+            // 
+            // lnameLabel
+            // 
+            this.lnameLabel.AutoSize = true;
+            this.lnameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnameLabel.Location = new System.Drawing.Point(36, 133);
+            this.lnameLabel.Name = "lnameLabel";
+            this.lnameLabel.Size = new System.Drawing.Size(73, 16);
+            this.lnameLabel.TabIndex = 12;
+            this.lnameLabel.Text = "Last Name";
+            // 
+            // fnameLabel
+            // 
+            this.fnameLabel.AutoSize = true;
+            this.fnameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fnameLabel.Location = new System.Drawing.Point(36, 107);
+            this.fnameLabel.Name = "fnameLabel";
+            this.fnameLabel.Size = new System.Drawing.Size(73, 16);
+            this.fnameLabel.TabIndex = 9;
+            this.fnameLabel.Text = "First Name";
+            // 
+            // fnameTextBox
+            // 
+            this.fnameTextBox.Location = new System.Drawing.Point(115, 106);
+            this.fnameTextBox.Name = "fnameTextBox";
+            this.fnameTextBox.Size = new System.Drawing.Size(160, 20);
+            this.fnameTextBox.TabIndex = 8;
+            // 
+            // studentComboBox
+            // 
+            this.studentComboBox.FormattingEnabled = true;
+            this.studentComboBox.Location = new System.Drawing.Point(115, 79);
+            this.studentComboBox.Name = "studentComboBox";
+            this.studentComboBox.Size = new System.Drawing.Size(160, 21);
+            this.studentComboBox.TabIndex = 30;
+            // 
+            // Student_Update_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 321);
+            this.Controls.Add(this.studentComboBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.degreeTextBox);
+            this.Controls.Add(this.degreeLabel);
+            this.Controls.Add(this.majorTextBox);
+            this.Controls.Add(this.majorLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lnameTextBox);
+            this.Controls.Add(this.lnameLabel);
+            this.Controls.Add(this.fnameTextBox);
+            this.Controls.Add(this.fnameLabel);
+            this.Name = "Student_Update_Form";
+            this.Text = "Student_Update_Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox degreeTextBox;
+        private System.Windows.Forms.Label degreeLabel;
+        private System.Windows.Forms.TextBox majorTextBox;
+        private System.Windows.Forms.Label majorLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox lnameTextBox;
+        private System.Windows.Forms.Label lnameLabel;
+        private System.Windows.Forms.Label fnameLabel;
+        private System.Windows.Forms.TextBox fnameTextBox;
+        private System.Windows.Forms.ComboBox studentComboBox;
+    }
+}
---
+++ Registration_System_Montelongo/Student_Update_Form.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registration_System_Montelongo {
+    public partial class Student_Update_Form : Form {
+        public Student_Update_Form() {
+            InitializeComponent();
+
+            using (MySqlConnection conn = new MySqlConnection("server=localhost;database=registration_db;user=root")) {
+                conn.Open();
+
+                string queryStudent = "SELECT lname, fname, student_id FROM student_table Order by lname, fname ASC;";
+                string querySection = "SELECT section_id, teacher_id, course_name, section, days, start_time, end_time FROM section_table Order by course_name, section ASC;";
+                MessageBox.Show(queryStudent);
+                MySqlCommand cmd = new MySqlCommand(queryStudent, conn);
+                MySqlCommand cmd2 = new MySqlCommand(querySection, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                DataTable tableStudent = new DataTable();
+                DataTable tableSection = new DataTable();
+
+                tableStudent.Load(reader);
+
+                for (int i = 0; i < tableStudent.Rows.Count; i++) {
+                    studentComboBox.Items.Add(tableStudent.Rows[i]["student_id"] + " " + tableStudent.Rows[i]["lname"] + "," + tableStudent.Rows[i]["fname"]);
+                }
+
+
+                reader = cmd2.ExecuteReader();
+                tableSection.Load(reader);
+
+                for (int i = 0; i < tableSection.Rows.Count; i++) {
+                    sectionComboBox.Items.Add(tableSection.Rows[i]["section_id"] + " : " + tableSection.Rows[i]["course_name"] + " " + tableSection.Rows[i]["section"]);
+                }
+
+                reader.Close();
+                conn.Close();
+
+            }
+
+        }
+    }
+}
